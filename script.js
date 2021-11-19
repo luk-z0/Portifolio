91334b9c080f86737329b14123c4353d594ff7da
Lucas Gabriel <70025847+luk-z0@users.noreply.github.com>
2021-11-19 17:18:21 -0300

    Att 1
    Adicionando os links e javascript inicial.

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("projeto1").addEventListener('click', () => {
+      window.open("https://github.com/luk-z0/LocadoraDeCarros");
+});
+document.getElementById("projeto2").addEventListener('click', () => {
+    window.open("http://pt.stackoverflow.com");
+});
+document.getElementById("projeto3").addEventListener('click', () => {
+    window.open("http://pt.stackoverflow.com");
+});
+
+
