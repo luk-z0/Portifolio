5d49468689401b4a286c78aeaffa462cf962035f
Lucas Gabriel <70025847+luk-z0@users.noreply.github.com>
2021-11-20 02:19:57 -0300

    Atualização 2
    Terminando a parte do Javascript, grande parte do CSS finalizado e faltando acrescentar algumas informações na aba "sobre" e "projetos".

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById("projeto1").addEventListener('click', () => {
-      window.open("https://github.com/luk-z0/LocadoraDeCarros");
+    window.open("https://github.com/luk-z0/LocadoraDeCarros");
 });
 document.getElementById("projeto2").addEventListener('click', () => {
     window.open("http://pt.stackoverflow.com");
@@ -8,4 +8,27 @@ document.getElementById("projeto3").addEventListener('click', () => {
     window.open("http://pt.stackoverflow.com");
 });
 
+let menu = document.querySelector(".menu-itens");
+let checkbox = document.getElementById("toggle");
 
+let show = true;
+
+menu.addEventListener("click", (event) => {
+    event.stopPropagation();
+
+
+    if (event.target.tagName === 'A' && !show) {
+        menu.style.visibility = 'hidden';
+        checkbox.checked = false;
+    }
+    if(checkbox.click()) {
+        menu.style.visibility = "visible";
+    }
+
+});
+
+let button = document.querySelector("#back-to-top");
+
+button.addEventListener("click", function() {
+    window.scrollTo(0, 0);
+});
